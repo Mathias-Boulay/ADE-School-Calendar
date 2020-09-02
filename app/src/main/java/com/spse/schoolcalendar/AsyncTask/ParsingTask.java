7d5b5839f13b685a2c3077dc93c49221dfe4d23b
SerpentSpirale <mathiasboulay@free.fr>
2020-09-02 19:12:51 +0200

    - Added ical librairies.

--- app/src/main/java/com/spse/schoolcalendar/AsyncTask/ParsingTask.java
+++ app/src/main/java/com/spse/schoolcalendar/AsyncTask/ParsingTask.java
@@ -3,13 +3,18 @@ package com.spse.schoolcalendar.AsyncTask;
 import android.os.AsyncTask;
 
 import com.spse.schoolcalendar.CalendarActivity;
+import net.fortuna.ical4j.data.CalendarBuilder;
+import net.fortuna.ical4j.model.*;
+import net.fortuna.ical4j.model.component.*;
+import net.fortuna.ical4j.model.property.*;
+import java.io.FileInputStream;
 
 public class ParsingTask extends AsyncTask<CalendarActivity,Void,Void> {
 
     @Override
     protected Void doInBackground(CalendarActivity... activity) {
 
-
+        
 
         return null;
     }
