1f6a93a3c2497fae378ba72ff81c494f3cbc567e
SerpentSpirale <mathiasboulay@free.fr>
2020-09-02 09:01:45 +0200

    - Started the whole structure of the app

---
+++ app/src/main/java/com/spse/schoolcalendar/MainActivity.java
@@ -0,0 +1,30 @@
+package com.spse.schoolcalendar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button b;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        new DownloadTask().execute("https://ade-web-consult.univ-amu.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?projectId=8&resources=8385&calType=ical&firstDate=2020-09-07&lastDate=2020-09-11",getAppDataPath() + "/test.ics");
+
+
+    }
+
+    public String getAppDataPath(){
+        return getApplicationInfo().dataDir;
+    }
+}
